5ccc49883357cb441d3790343d381991cc921274
ankitsingh <ankit@ost.com>
2019-12-14 22:18:01 +0530

    initial commit with some basic boilerplate code.

---
+++ set_env_vars.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Core ENV Details
+export WP_ENVIRONMENT='development'
+export WP_PORT=3000
+export WP_COOKIE_DOMAIN='localhost'
+export WP_APP_NAME='casaone'
+export WP_COOKIE_SECRET='aa5298d3a3fe181a3a52d085ee1525df5asa498337f8f3b76ca7df0a5de3211b'
+export MSG91_AUTH_KEY='303883A8nRvJvPg5dcd9833'
+export MSG91_OTP_TEMPLATE='5dce45aed6fc054f945b773a'
